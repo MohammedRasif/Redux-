a1c9fc3857101e0e5b8821e04558d465c4b48502
mohammedRasif <mdrasif3499@gmail.com>
2025-02-15 11:24:10 +0600

    create login page

--- src/components/Shared/Login.jsx
+++ src/components/Shared/Login.jsx
@@ -1,8 +1,18 @@
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login section</h1>
+        <div className="m-10">
+            <div>
+                <h1 className="text-xl font-bold">Email</h1>
+                <input type="email" name="" className="border-2 py-2 px-20" id="" />
+            </div>
+            <div className="mt-3">
+                <h1 className="text-xl font-bold">Password</h1>
+                <input type="email" name="" className="border-2 py-2 px-20" id="" />
+            </div>
+
+            <button className="text-md font-bold px-3 py-2 bg-black text-white mt-5">Submit</button>
+
         </div>
     );
 }
