b5c8a7f78dac46a51938cded277e69870333f1b8
mohammedRasif <mdrasif3499@gmail.com>
2025-02-15 11:26:00 +0600

    register page create

--- src/components/Shared/Register.jsx
+++ src/components/Shared/Register.jsx
@@ -2,7 +2,23 @@
 const Register = () => {
     return (
         <div>
-            <h1>this is register section</h1>
+           <div className="m-10">
+            <div>
+                <h1 className="text-xl font-bold">User</h1>
+                <input type="text" name="" className="border-2 py-2 px-20" id="" />
+            </div>
+            <div>
+                <h1 className="text-xl font-bold">Email</h1>
+                <input type="email" name="" className="border-2 py-2 px-20" id="" />
+            </div>
+            <div className="mt-3">
+                <h1 className="text-xl font-bold">Password</h1>
+                <input type="email" name="" className="border-2 py-2 px-20" id="" />
+            </div>
+
+            <button className="text-md font-bold px-3 py-2 bg-black text-white mt-5">Submit</button>
+
+        </div>
         </div>
     );
 }
